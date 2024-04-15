8c79cce810ae7d1fba4ffa1efedd59aa8783d4d5
noahkise <noah@kisefamily.com>
2024-04-15 13:39:18 -0700

    begin day 1 dedicated capstone section

--- src/components/Discogs.jsx
+++ src/components/Discogs.jsx
@@ -1,14 +1,3 @@
-const Discogs = async (bandName) => {
+import { useState } from 'react';
 
-    const cityCode = await getCityId(city, state)
-    try {
-        const response = await fetch(`https://www.jambase.com/jb-api/v1/events?artistName=${bandName}&geoCityId=${cityCode}&apikey=${import.meta.env.VITE_REACT_APP_JAMBASE}`);
-        if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-        }
-        const jsonifiedresponse = await response.json();
-        return jsonifiedresponse.events;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-};
+
