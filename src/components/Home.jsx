9bcde5d17f5732c85c2f5aadf364b2eef15c21e2
noahkise <noah@kisefamily.com>
2024-04-15 15:04:55 -0700

    leaderboard or login conditionaly rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,6 +28,7 @@ export default function Home() {
     const [resetSuccess, setResetSuccess] = useState(null);
     const [forgotPassword, setForgotPassword] = useState(false);
     const [leaderboard, setLeaderboard] = useState([]);
+    const [userId, setUserId] = useState('');
     const navigate = useNavigate();
 
     const handleImageChange = (e) => {
@@ -90,151 +91,138 @@ export default function Home() {
     };
 
     useEffect(() => {
+        const checkCurrentUser = async () => {
+            try {
+                auth.onAuthStateChanged((user) => {
+                    if (user) {
+                        setUserId(user.uid || '');
+                    }
+                });
+            } catch (error) {
+                console.log("error fetching current user")
+            }
+        };
+
+        checkCurrentUser();
+    }, []);
+
+    useEffect(() => {
         const fetchLeaderboard = async () => {
             const scoresCollection = collection(db, "score");
             const snapshot = await getDocs(scoresCollection);
             const leaderboardData = [];
-            snapshot.forEach(doc => {
-                const userId = doc.id; // Assuming userId is the document ID
-                const scoreData = doc.data();
-                leaderboardData.push({ userId, ...scoreData });
+
+            // Fetch usernames for each userId
+            const userIds = snapshot.docs.map(doc => doc.id);
+            const usersCollection = collection(db, "users");
+            const usersSnapshot = await getDocs(usersCollection);
+            const userIdToUsernameMap = {};
+            usersSnapshot.forEach(doc => {
+                const userId = doc.id;
+                const username = doc.data().username;
+                userIdToUsernameMap[userId] = username;
             });
-            console.log(leaderboardData);
+            snapshot.forEach(doc => {
+                const userId = doc.id;
+                const scoreData = doc.data();
+                const username = userIdToUsernameMap[userId] || "Unknown"; // Default to "Unknown" if username not found
+                leaderboardData.push({ username, ...scoreData });
+            });
+            leaderboardData.sort((a, b) => b.score - a.score);
             setLeaderboard(leaderboardData);
         };
+
         fetchLeaderboard();
     }, []);
 
     return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar id='avatar' sx={{ m: 1, bgcolor: "secondary.main" }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography id="signInH1" component="h1" variant="h5">
-                        {showSignUp ? "Sign Up" : "Sign In"}
-                    </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={showSignUp ? doSignUp : doSignIn}
-                        noValidate
-                        sx={{ mt: 1 }}
-                    >
-                        {showSignUp && (
-                            <>
-                                <TextField
-                                    type='text'
-                                    name='email'
-                                    placeholder='Email'
-                                    fullWidth
-                                />
-                                <TextField
-                                    type="text"
-                                    name="username"
-                                    placeholder="Username"
-                                    fullWidth
-                                />
-                                <TextField
-                                    minLength="6"
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
-                                    fullWidth
-                                />
-                                <br />
-                                Profile Image:
-                                <br />
-                                <CustomFileInput onChange={handleImageChange} />
-                                <br />
-                            </>
-                        )}
-                        {!showSignUp && (
-                            <>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="signinEmail"
-                                    label="Email Address"
-                                    name="signinEmail"
-                                    autoComplete="email"
-                                    autoFocus
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="signinPassword"
-                                    label="Password"
-                                    type="password"
-                                    id="signinPassword"
-                                    autoComplete="current-password"
-                                />
-                            </>
-                        )}
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            {showSignUp ? "Sign Up" : "Sign In"}
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                {forgotPassword ? (
+        <>
+            {userId ? (
+                <>
+                    <div id="leaderboard">
+                        <h1>LEADERBOARD</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Rank</th>
+                                    <th>Username</th>
+                                    <th>Score</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {leaderboard.map((entry, index) => (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{entry.username}</td>
+                                        <td>{entry.score}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </>
+            ) : (
+                <ThemeProvider theme={defaultTheme}>
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <Avatar id='avatar' sx={{ m: 1, bgcolor: "secondary.main" }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography id="signInH1" component="h1" variant="h5">
+                                {showSignUp ? "Sign Up" : "Sign In"}
+                            </Typography>
+                            <Box component="form" onSubmit={showSignUp ? doSignUp : doSignIn} noValidate sx={{ mt: 1 }}>
+                                {showSignUp && (
                                     <>
-                                        <TextField
-                                            type="text"
-                                            name="resetEmail"
-                                            placeholder="Enter your email"
-                                            value={resetEmail}
-                                            onChange={(e) => setResetEmail(e.target.value)}
-                                        />
+                                        <TextField type='text' name='email' placeholder='Email' fullWidth />
+                                        <TextField type="text" name="username" placeholder="Username" fullWidth />
+                                        <TextField minLength="6" type="password" name="password" placeholder="Password" fullWidth />
                                         <br />
-                                        <Button
-                                            type="button"
-                                            variant="contained"
-                                            onClick={doPasswordReset}
-                                        >
-                                            Reset Password
-                                        </Button>
-                                        {resetSuccess && <p>{resetSuccess}</p>}
+                                        Profile Image:
+                                        <br />
+                                        <CustomFileInput onChange={handleImageChange} />
+                                        <br />
                                     </>
-                                ) : (
-                                    <Link href="#" variant="body2" onClick={handleForgotPassword}>
-                                        Forgot password?
-                                    </Link>
                                 )}
-                            </Grid>
-                            <Grid item>
-                                <Link
-                                    href="#"
-                                    variant="body2"
-                                    onClick={() => setShowSignUp(!showSignUp)}
-                                >
-                                    {showSignUp
-                                        ? "Already have an account? Sign In"
-                                        : "Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
+                                {!showSignUp && (
+                                    <>
+                                        <TextField margin="normal" required fullWidth id="signinEmail" label="Email Address" name="signinEmail" autoComplete="email" autoFocus />
+                                        <TextField margin="normal" required fullWidth name="signinPassword" label="Password" type="password" id="signinPassword" autoComplete="current-password" />
+                                    </>
+                                )}
+                                <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+                                <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                                    {showSignUp ? "Sign Up" : "Sign In"}
+                                </Button>
+                                <Grid container>
+                                    <Grid item xs>
+                                        {forgotPassword ? (
+                                            <>
+                                                <TextField type="text" name="resetEmail" placeholder="Enter your email" value={resetEmail} onChange={(e) => setResetEmail(e.target.value)} />
+                                                <br />
+                                                <Button type="button" variant="contained" onClick={doPasswordReset}>
+                                                    Reset Password
+                                                </Button>
+                                                {resetSuccess && <p>{resetSuccess}</p>}
+                                            </>
+                                        ) : (
+                                            <Link href="#" variant="body2" onClick={handleForgotPassword}>
+                                                Forgot password?
+                                            </Link>
+                                        )}
+                                    </Grid>
+                                    <Grid item>
+                                        <Link href="#" variant="body2" onClick={() => setShowSignUp(!showSignUp)}>
+                                            {showSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Box>
+                        </Box>
+                    </Container>
+                </ThemeProvider>
+            )}
+        </>
     );
 }
