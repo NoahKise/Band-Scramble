5a908dfa7c576acd78ab970346da0810d3abef08
noahkise <noah@kisefamily.com>
2024-05-01 10:10:38 -0700

    hide navbar when not signed in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -7,11 +6,40 @@ import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { auth, db } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import '../App.css';
 
 export const Navbar = () => {
     const [value, setValue] = React.useState(0);
+    const [username, setUsername] = useState('');
     const navigate = useNavigate();
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                auth.onAuthStateChanged(async (user) => {
+                    if (user) {
+                        const userId = user.uid;
+                        const userRef = doc(db, 'users', userId);
+                        const userSnapshot = await getDoc(userRef);
+
+                        if (userSnapshot.exists()) {
+                            const userData = userSnapshot.data();
+                            setUsername(userData.username)
+                        } else {
+                            console.log("User not found!");
+                        }
+                    }
+                });
+            } catch (error) {
+                console.log('error fetching user data');
+            }
+        };
+        fetchData();
+    }, []);
 
     const handleNavigation = (index) => {
         setValue(index);
@@ -35,7 +63,7 @@ export const Navbar = () => {
     };
 
     return (
-        <div id='navbar'>
+        <div id='navbar' style={{ display: username ? '' : 'none' }}>
             <Box sx={{ width: 400 }} >
                 <BottomNavigation
                     showLabels
